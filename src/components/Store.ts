4e17ebf0047c2f3e15327da7cb387dbef915c59a
dhiiyaur <dhiyarifqi@gmail.com>
2021-10-21 23:03:51 +0700

    fix bug local storage

--- src/components/Store.ts
+++ src/components/Store.ts
@@ -2,11 +2,17 @@ import { writable } from 'svelte/store';
 
 // export const dataChapter = writable({});
 
-const itemName = "dataChapter"
-const retrieved = localStorage.getItem(itemName)
-const parsed = JSON.parse(retrieved)
-export const dataChapter = writable(parsed === null ? [] : parsed)
+const itemName = 'dataChapter';
+// const retrieved = localStorage.getItem(itemName)
+const retrieved = () => {
+	if (typeof window !== 'undefined') {
+		return localStorage.getItem(itemName);
+	} else {
+		return null;
+	}
+};
 
-dataChapter.subscribe(value =>
-  localStorage.setItem(itemName, JSON.stringify(value))
-)
+const parsed = JSON.parse(retrieved());
+export const dataChapter = writable(parsed === null ? [] : parsed);
+
+dataChapter.subscribe((value) => localStorage.setItem(itemName, JSON.stringify(value)));
