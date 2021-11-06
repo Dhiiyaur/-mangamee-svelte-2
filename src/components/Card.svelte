b97c067d4800ff5b2d9cdf6a79b508520fa593eb
dhiiyaur <dhiyarifqi@gmail.com>
2021-11-06 17:05:00 +0700

    add

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,78 +1,34 @@
 <script lang="ts">
-	export let cardData: any
-	let mangaData = cardData["MangaData"];
-	let mangaSource = cardData["MangaSource"];
+	export let cardData: any, browseMode: boolean
+	let mangaData = cardData['MangaData'];
+	let mangaSource = cardData['MangaSource'];
 </script>
 
 {#each mangaData as manga}
 	<div class="bg-gray-600 sm:w-56 w-28 rounded-md shadow-lg flex flex-col justify-between gap-x-2">
 		<div>
-			<a href={`/manga/${mangaSource}/${manga["MangaLink"]}`}>
+			<a href={`/manga/${mangaSource}/${manga['MangaLink']}`}>
 				<div>
-					<img src={manga["MangaCover"]} alt="" class="w-full h-44 sm:h-64 object-cover rounded-t-md" />
+					<img
+						src={manga['MangaCover']}
+						alt=""
+						class="w-full h-44 sm:h-64 object-cover rounded-t-md"
+					/>
 				</div>
 				<div>
-					<div class="py-4 p-2 text-sm text-white">{manga["MangaTitle"]}</div>
+					<div class="py-4 p-2 text-sm text-white">{manga['MangaTitle']}</div>
 				</div>
 			</a>
 		</div>
+
+		{#if !browseMode}
+			<div class="text-right text-white text-xs pr-3 pb-2">
+				Last Update - {manga["MangaLastChapter"]}
+			</div>
+		{/if}
 	</div>
 {/each}
 
-
-<!-- <div class="grid grid-cols-3 sm:grid-cols-6 sm:gap-y-4 sm:gap-x-1 gap-4 py-28">
-	{#each mangaData as manga}
-		<div
-			class="bg-gray-600 sm:w-56 w-28 rounded-md shadow-lg flex flex-col justify-between gap-x-2"
-		>
-			<div>
-				<a href={`/manga/${mangaSource}/${manga.MangaLink}`}>
-					<div>
-						<img
-							src={manga.MangaCover}
-							alt=""
-							class="w-full h-44 sm:h-64 object-cover rounded-t-md"
-						/>
-					</div>
-					<div>
-						<div class="py-4 p-2 text-sm text-white">{manga.MangaTitle}</div>
-					</div>
-				</a>
-			</div>
-			<div> -->
-				<!-- {#if lastchapterMenu}
-			<div class="text-right text-white text-xs pr-3 pb-2">
-				Last Update - {dataManga.MangaLastChapter}
-			</div>
-		{/if}
-		{#if userHistoryExt}
-			<div class="text-right text-white text-xs truncate pr-2 pl-4">
-				Last Update - {dataManga.MangaLastChapter}
-			</div>
-			<div class="text-right text-white text-xs truncate pr-2 pl-4">
-				Last Read - {dataManga.MangaLastRead}
-			</div>
-		{/if} -->
-			<!-- </div>
-			<div> -->
-				<!-- {#if userHistoryExt}
-			<div class="justify-center flex pb-2 pt-2">
-				<button
-					class="p-1 text-red-500 text-sm shadow rounded bg-white"
-					on:click={(e) => {
-						handleDeleteUserManga(jwtUser, dataManga.MangaLink);
-					}}
-				>
-					Delete
-				</button>
-			</div>
-		{/if} -->
-
-		
-			<!-- </div>
-		</div>
-	{/each}
-</div> -->
 
 <!-- <script lang="ts">
 	export let data: any;
